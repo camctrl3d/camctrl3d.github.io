1d065916710d3f63ad7115c9a22dfd9e5defed61
Stefan Popov <stefan@popov.im>
2025-03-20 00:08:38 +0100

    Add swipe support

--- src/video_display.tsx
+++ src/video_display.tsx
@@ -1,6 +1,35 @@
 import * as React from "react";
 import styles from "./video_display.module.scss";
 
+interface SwipeEventHandlers extends Partial<GlobalEventHandlers> {
+  onTouchStart: React.TouchEventHandler;
+  onTouchEnd: React.TouchEventHandler;
+}
+
+function useSwipe(
+  onSwipeLeft: () => void, onSwipeRight: () => void,
+  distanceThreshold = 50, timeThresholdMs = 500
+): SwipeEventHandlers {
+  const startX = React.useRef(0);
+  const startTime = React.useRef(0);
+
+  const onTouchStart: React.TouchEventHandler = React.useCallback(
+    (e) => {
+      startX.current = e.touches[0].clientX;
+      startTime.current = Date.now();
+    }, [startX, startTime]);
+
+  const onTouchEnd: React.TouchEventHandler = React.useCallback(
+    (e) => {
+      const dist = startX.current - e.changedTouches[0].clientX;
+      const duration = Date.now() - startTime.current;
+      if (Math.abs(dist) > distanceThreshold && duration < timeThresholdMs) {
+        if (dist > 0) onSwipeLeft(); else onSwipeRight();
+      }
+    }, [onSwipeLeft, onSwipeRight, startX, startTime]);
+  return { onTouchStart, onTouchEnd };
+};
+
 type ElementSize = { width?: number; height?: number };
 export function useElementSize<T extends HTMLElement>(): [ElementSize, React.RefObject<T>] {
   const ref = React.useRef<T | null>(null);
@@ -60,6 +89,8 @@ export function Carousel(props: {
   const [vidIdx, updIdx] = React.useReducer(
     (x: number, action: { offset: number, is_absolute: boolean }) =>
       action.is_absolute ? action.offset : (x + action.offset + items) % items, 0);
+  const navNextVideo = React.useCallback(() => updIdx({ offset: 1, is_absolute: false }), [updIdx]);
+  const navPrevVideo = React.useCallback(() => updIdx({ offset: -1, is_absolute: false }), [updIdx]);
 
   React.useEffect(() => {
     if (props.onVideoChanged) props.onVideoChanged(vidIdx);
@@ -85,13 +116,15 @@ export function Carousel(props: {
     </div>;
   });
 
+  const swipeEvents = useSwipe(navNextVideo, navPrevVideo);
+
   return (
-    <div className={styles.video_slider + " " + (props.className || "")}>
+    <div className={styles.video_slider + " " + (props.className || "")} {...swipeEvents} >
       {mainElems}
       <div style={navGridCss} className={styles.video_slider_nav}>
-        <NavButton direction="left" onClick={() => updIdx({ offset: -1, is_absolute: false })} />
+        <NavButton direction="left" onClick={navPrevVideo} />
         {thumbs}
-        <NavButton direction="right" onClick={() => updIdx({ offset: 1, is_absolute: false })} />
+        <NavButton direction="right" onClick={navNextVideo} />
       </div>
     </div>
   );
